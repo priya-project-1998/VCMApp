ff63db1e496ad311cd1ee87fc320ae84d3b5718d
Raja.Jain <raja.jain@wolterskluweer.com>
2025-08-02 23:51:38 +0530

    Update project structure

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SplashScreen({ navigation }) {
+useEffect(() => {
+  setTimeout(() => {
+    navigation.replace('Login');
+  }, 2000);
+}, []);
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>VCM App Loading...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 24 },
+});
