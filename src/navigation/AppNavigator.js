657d7e125279b32b23c502d1bb5dbd7576d5374a
priyakeshari <p3219400@gmail.com>
2025-08-03 13:40:13 +0530

    merging latest code with drwaer navigation, login and signup fixes for dev

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen.js'
-import LoginScreen from '../screens/LoginScreen.js';
-import SignupScreen from '../screens/SignupScreen.js';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
-
+const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+
+    </Drawer.Navigator>
+  );
+}
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-
-      <Stack.Navigator>
-        <Stack.Screen name="SignupScreen" component={SignupScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Drawer" component={DrawerNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
